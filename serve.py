317f0bdfc4610941e60421de01d647fc9555b918
Yuxiang Gao <yuxiang.gao@jhu.edu>
2022-02-04 12:26:30 -0500

    Cleanup serve script

--- serve.py
+++ serve.py
@@ -1,12 +1,11 @@
+#! /usr/bin/env python3
 import os
 import threading
 from functools import partial
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from pathlib import Path
-from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 os.environ["DISPLAY"] = ":0"
 PORT = 8000
@@ -14,25 +13,24 @@ PORT = 8000
 Handler = SimpleHTTPRequestHandler
 
 file_path = Path(__file__).resolve(strict=True).parent
-cmds = [
-    f"devilspie2 --folder={file_path} --debug",
-    f"DISPLAY=:0 && x-www-browser http://0.0.0.0:{PORT}",
-]
+# cmds = [
+#     f"devilspie2 --folder={file_path} --debug",
+#     f"DISPLAY=:0 && x-www-browser http://0.0.0.0:{PORT}",
+# ]
 
-
+# start htt server
 handler = partial(SimpleHTTPRequestHandler, directory=str(file_path))
 server = ThreadingHTTPServer(("", PORT), handler)
-
-server_thread = threading.Thread(target=server.serve_forever)
-server_thread.daemon = True
+server_thread = threading.Thread(target=server.serve_forever, daemon=True)
 server_thread.start()
-driver = webdriver.Firefox()
 
-driver.get(f"http://0.0.0.0:{PORT}")
+# start browser
+driver = webdriver.Firefox()
 driver.fullscreen_window()
+driver.get(f"http://0.0.0.0:{PORT}")
 driver.execute_script(
     "var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;"
 )
-while True:
-    sleep(1)
+
+server_thread.join()
 driver.quit()
